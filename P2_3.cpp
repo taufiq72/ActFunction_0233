ba519e9f980d3ceb58a2bec785cc92bfa4c04e7c
taufiq72 <taufiqq1207@gmail.com>
2026-03-03 20:51:57 +0700

    Prosedur input

--- P2_3.cpp
+++ P2_3.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 //deklarasi global
 int p, l;
+
+// Implementasi Prosuder dan Fungsi
+void input(){
+    cout << "Masukan Panjang = ";
+    cin >> p;
+    cout << "Masukan Lebar = ";
+    cin >> l;
+}
